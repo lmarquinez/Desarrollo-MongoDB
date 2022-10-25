5a085294019a1f21723e4b687a7fe26f16e61a8f
Lauri <lauri.marquinez@gmail.com>
2022-10-26 01:30:58 +0200

    CRUD

--- inmueblesApp/routes/api/properties.js
+++ inmueblesApp/routes/api/properties.js
@@ -1,9 +1,44 @@
 const router = require('express').Router();
 
+/* Importing the property model. */
+const Property = require("../../models/property.model");
+
 router.get('/', async (req, res) => {
+    try {
+        const properties = await Property.find();
+        res.json(properties);
+    } catch (error) {
+        res.json({ Error: error.message });
+    }
+});
 
-    res.send('I recover the properties');
+router.post("/", async (req, res) => {
+    try {
+        const properties = await Property.create(req.body);
+        res.json(properties);
+    } catch (error) {
+        res.json({ Error: error.message });
+    }
+});
 
+router.put('/:propertyId', async (req, res) => {
+    const { propertyId } = req.params;
+    try {
+        const property = await Property.findByIdAndUpdate(propertyId, req.body, { new: true });
+        res.json(property);
+    } catch (error) {
+        res.json({ Error: error.message });
+    }
+});
+
+router.delete('/:propertyId', async (req, res) => {
+    const { propertyId } = req.params;
+    try {
+        const property = await Property.findByIdAndDelete(propertyId);
+        res.json(property);
+    } catch (error) {
+        res.json({ Error: error.message });
+    }
 });
 
 
